a0e53baf27f59cf230cb7ebddc68dfe853117535
Sathvik Chowdary Veerapaneni <sathvikchowdaryveerapaneni@gmail.com>
2024-06-03 20:16:19 -0500

    Modiefied according to Flask

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('skills.html')
+
+@app.route('/work')
+def work():
+    return render_template('work.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
